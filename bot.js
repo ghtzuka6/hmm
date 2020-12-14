8fc8150e960570b9b8f8636f02e1604968bcf0d3
yngandrew <conturigobbz@gmail.com>
2020-12-14 15:46:23 +0200

    Added sequelize

--- bot.js
+++ bot.js
@@ -10,85 +10,99 @@ const { readdirSync } = require('fs');
 
 const { join } = require('path');
 
-client.commands= new Discord.Collection();
+client.commands = new Discord.Collection();
 
 const prefix = 'c!';
 //You can change the prefix if you like. It doesn't have to be ! or ;
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('database', 'user', 'password', {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: "false",
+
+  storage: "database.sqlite",
+});
 
 
 const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-    const command = require(join(__dirname, "commands", `${file}`));
-    client.commands.set(command.name, command);
+  const command = require(join(__dirname, "commands", `${file}`));
+  client.commands.set(command.name, command);
 }
 
 
 client.on("error", console.error);
 
 client.on('ready', () => {
-    console.log('I am ready');
+  console.log('I am ready');
 });
 
 
 
 
 function clean(text) {
-    if (typeof(text) === "string")
-      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-    else
-        return text;
-  }
+  if (typeof (text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+    return text;
+}
 
 
 client.on("message", message => {
-    const args = message.content.split(" ").slice(1);
-   
-    if (message.content.startsWith(config.prefix + "eval")) {
-        if(message.author.id !== config.ownerID) return;
-      try {
-        const code = args.join(" ");
-        let evaled = eval(code);
-   
-        if (typeof evaled !== "string")
-          evaled = require("util").inspect(evaled);
-   
-        message.channel.send(clean(evaled), {code:"xl"});
-      } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-      }
+  const args = message.content.split(" ").slice(1);
+
+  if (message.content.startsWith(config.prefix + "eval")) {
+    if (message.author.id !== config.ownerID) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), { code: "xl" });
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
     }
- });
-  
- client.on("message", async message => {
-    
-    if (message.mentions.users.get(client.user.id))  {
-        message.channel.send({ embed: {
-            title: `salut, ${message.member},`,
-            description: `prefix-ul meu este **${prefix}**`,
-        }})
-    }})
+  }
+});
 
-client.on("message", async message => {
+  client.on("message", async message => {
 
-    if(message.author.bot) return;
-    if(message.channel.type === 'dm') return;
-
-    if(message.content.startsWith(prefix)) {
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
-
-        const command = args.shift().toLowerCase();
-
-        if(!client.commands.has(command)) return;
-
-
-        try {
-            client.commands.get(command).run(client, message, args);
-
-        } catch (error){
-            console.error(error);
+    if (message.mentions.users.get(client.user.id)) {
+      message.channel.send({
+        embed: {
+          title: `salut, ${message.author},`,
+          description: `prefix-ul meu este **${prefix}**`,
         }
+      })
     }
-})
+  })
 
-client.login(token);
+  client.on("message", async message => {
+
+    if (message.author.bot) return;
+    if (message.channel.type === 'dm') return;
+
+    if (message.content.startsWith(prefix)) {
+      const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+      const command = args.shift().toLowerCase();
+
+      if (!client.commands.has(command)) return;
+
+
+      try {
+        client.commands.get(command).run(client, message, args);
+
+      } catch (error) {
+        console.error(error);
+      }}
+    })
+
+    
+
+  client.login(token);
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: 'Nzc3NTM2MTMyMjU0NjYyNjc3.X7E28w.o8yP3htbPa-kCqa-ppr87BK31do' });
+const manager = new ShardingManager('./bot.js', { token: 'Nzc3NTM2MTMyMjU0NjYyNjc3.X7E28w.POzmZe4yGBCF0ryTpUIcULz7cFo' });
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
